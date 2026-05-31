55f24938ea75c3d4aace2588dee46c6352318b7e
Kevin Stadnick <kevinstadnicko@gmail.com>
2026-05-31 20:48:25 -0300

    Fim do DB da evence

--- teste-backend-Evence/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListaEventosComponent } from './pages/lista-eventos.component/lista-eventos.component';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: ListaEventosComponent
-    }
-];
